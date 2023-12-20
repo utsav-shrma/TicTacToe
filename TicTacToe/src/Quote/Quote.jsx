a02e05a8585f1017088140967e2424d1a173ce34
Utsav Sharma <utsav19913@gmail.com>
2023-12-20 17:48:09 +0530

    Added dynamic and persitent quote count

--- TicTacToe/src/Quote/Quote.jsx
+++ TicTacToe/src/Quote/Quote.jsx
@@ -5,19 +5,53 @@ import { useState,useEffect } from "react";
 
 
 function Quote() {
-    const [quote, setQuote] = useState('loading ...');
+    let [quote, setQuote] = useState('loading ...');
+    let [quoteCount,setQuoteCount]=useState(0);
+
+  
+    useEffect(()=>{
+      
+      
+      const storedQuote = JSON.parse(window.localStorage.getItem('quote'));
+      const storedQuoteCount = JSON.parse(window.localStorage.getItem('quoteCount'));
+
+      if (storedQuote && storedQuoteCount) {
+        setQuoteCount(JSON.parse(window.localStorage.getItem('quoteCount')));
+        setQuote(JSON.parse(window.localStorage.getItem('quote')));}
+      else{
+        window.localStorage.setItem('quote', JSON.stringify(quote));
+        window.localStorage.setItem('quoteCount', JSON.stringify(quoteCount));
+
+      }
+
+      fetchQuote();
+      
+      }, []);
+
+    useEffect(() => {
+      window.localStorage.setItem('quote', JSON.stringify(quote));
+    }, [quote]);
+
+    useEffect(() => {
+      
+      window.localStorage.setItem('quoteCount', JSON.stringify(quoteCount));
+    }, [quoteCount]);
+  
 
     const fetchQuote=()=>{
         fetch('	https://api.adviceslip.com/advice')
         .then((res)=>{return res.json();}).then((data)=>{ setQuote(data.slip.advice);})
         .catch((err)=>{console.log(err)});
+
+        setQuoteCount(JSON.parse(window.localStorage.getItem('quoteCount'))   +1);
+        
     }
 
-    useEffect(()=>{fetchQuote();}, []);
+   
 
   return (
     <div id="quote-container">
-    <h1>Quote #1</h1>
+    <h1>Quote #{quoteCount}</h1>
     <p>
       {quote}
     </p>
